9ab12bb6455fb903e4ddd51aeb8a9b84c6931272
greg <greg@65e1e2d5-ad67-db11-b036-0011d808431b>
2006-12-04 22:59:06 +0000

    version 0.3
    git-svn-id: file:///home/svn/s3c/trunk@53 65e1e2d5-ad67-db11-b036-0011d808431b

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name = "s3lib",
-    version = "0.2",
+    version = "0.3",
     description = "Amazon S3 Interface Library",
     author = "Greg Hewgill",
     author_email = "greg@hewgill.com",
